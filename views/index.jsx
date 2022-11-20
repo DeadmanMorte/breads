fa7080607eed0cea5741ac09842fa314f62eab9a
Isaac Kim <kisaac95@yahoo.com>
2022-11-20 14:19:55 -0800

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -1,24 +1,23 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index ( {breads} ) {
+function Index ( {breads, title} ) {
     return (
-      <Default>
+      <Default title = {title}>
         <h2>Index Page</h2>
         {/* <p> I have {breads[0].name} bread!</p> */}
         <ul>
         {
-  breads.map((bread, index) => {
-    return (
-      <li key={index}>
-        <a href={`/breads/${index}`}>
-          {bread.name}
-        </a>
-      </li>
-    )
-  })
-}
-
+        breads.map((bread, index) => {
+            return (
+            <li key={index}>
+                <a href={`/breads/${index}`}>
+                {bread.name}
+                </a>
+            </li>
+            )
+        })
+        }
         </ul>
       </Default>
     )
